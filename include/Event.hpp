cb40240230b9d99a9d640112cb35e11691740326
Vilho Luoma <vilho.luoma@gmail.com>
2024-10-26 14:43:22 +0300

    store time_point instead and get timestamp elsewhere

--- include/Event.hpp
+++ include/Event.hpp
@@ -15,6 +15,7 @@ class Event
   ~Event();
 
   std::string getEventType() const;
+  std::string getEventTime() const;
 
 private:
   std::string formatTime(const std::chrono::steady_clock::time_point &event_time) const;
@@ -22,8 +23,8 @@ private:
  public:
   int watch_descriptor;
   uint32_t mask;
-  std::filesystem::path path;
-  const std::string timestamp;
+  const std::filesystem::path path;
+  const std::chrono::steady_clock::time_point event_time;
 };
 }  // namespace inotify
 
--- src/Event.cpp
+++ src/Event.cpp
@@ -4,7 +4,7 @@ inotify::Event::Event(int watch_descriptor,
     uint32_t mask,
     const std::filesystem::path& path,
     const std::chrono::steady_clock::time_point& event_time)
-  : watch_descriptor(watch_descriptor), mask(mask), path(path), timestamp(formatTime(event_time))
+  : watch_descriptor(watch_descriptor), mask(mask), path(path), event_time(event_time)
 {
 }
 
@@ -18,5 +18,10 @@ std::string inotify::Event::formatTime(
   std::stringstream ss;
   ss << std::put_time(std::localtime(&time_t_format), "%Y-%m-%d %H:%M:%S");
   return ss.str();
+}
+
+std::string inotify::Event::getEventTime() const
+{
+  return formatTime(event_time);
 }
 
